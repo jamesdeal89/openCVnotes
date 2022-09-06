68e220965ea7822ff314a01887822716887a0ad7
James Deal <j891319@protonmail.com>
2022-09-06 10:45:11 +0800

    updated color loading

--- openCV1.py
+++ openCV1.py
@@ -2,14 +2,14 @@
 import cv2
 
 # images can be imported like so
-img = cv2.imread('oldMan.jpeg', 0)
+img = cv2.imread('oldMan.jpeg', -1)
 # there are several ways to process an image
 # these include color, greyscale, and unchanged
 # these are signified as:
 # -1, cv2.IMREAD_COLOR
 # 0, cv2.IMREAD_GREYSCALE
 # 1, cv2.IMREAD_INCHANGED
-# above I have put 0 as the second argument; meaning greyscale
+# above I have put -1 as the second argument; meaning color
 
 # images can be resized using cv2.resize and passing in a loaded image and a tuple for resolution
 img = cv2.resize(img,(400,400))
