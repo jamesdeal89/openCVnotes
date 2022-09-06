4d0bbe55812e0963cdc9e21c4de66a2bc6dc8b11
James Deal <j891319@protonmail.com>
2022-09-06 11:48:19 +0800

    updated webcam

--- openCV2.py
+++ openCV2.py
@@ -5,16 +5,19 @@ import cv2
 # this is done in individual video frames being processed
 # a webcam feed can be used by passing in 0 for the default webcam 
 # a saved video file uses a string of the filepath
-capture = cv2.VideoCapture('cat.mp4')
+capture = cv2.VideoCapture(0)
 
-# using a loop we can iterate through frames
-while True:
-    # .read() returns a tuple of a boolean and the image frame
-    isTrue, frame = capture.read()
-    # if there is a frame
-    if isTrue:
-        # display the frame
-        cv2.imshow('video',frame)
+# get the first frame
+isTrue, frame = capture.read()
+# resize the frame 
+frame = cv2.resize(frame,(600,400))
+
+# using a loop we can iterate through frames if the first frame was succesful
+while isTrue:
+    # display the frame
+    cv2.imshow('video',frame)
+    # get next frame
+    frame = cv2.resize(frame,(600,400))
     # wait for any keypress and stop if so
     if cv2.waitKey(0):
         break
