2ca3b7a0d8b47b5e5465555dbfab9686d44049b7
James Deal <j891319@protonmail.com>
2022-09-06 10:36:18 +0800

    added first file

---
+++ openCV1.py
@@ -0,0 +1,21 @@
+# first tutorial chapter for openCV 
+import cv2
+
+# images can be imported like so
+img = cv2.imread('oldMan.jpeg', 0)
+# there are several ways to process an image
+# these include color, greyscale, and unchanged
+# these are signified as:
+# -1, cv2.IMREAD_COLOR
+# 0, cv2.IMREAD_GREYSCALE
+# 1, cv2.IMREAD_INCHANGED
+# above I have put 0 as the second argument; meaning greyscale
+
+# images can be resized using cv2.resize and passing in a loaded image and a tuple for resolution
+img = cv2.resize(img,(400,400))
+
+cv2.imshow('image', img)
+# waits for a keypress. 0 means infinitely, an integer means a sepcificed time until skipped
+cv2.waitKey(0)
+# closes all the windows made by cv2
+cv2.destroyAllWindows()
