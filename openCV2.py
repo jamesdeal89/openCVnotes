08473b5e1e947ab9bd3ed6d771e9e6cef33b2026
James Deal <j891319@protonmail.com>
2022-09-07 08:51:51 +0800

    updated error in file 2

--- openCV2.py
+++ openCV2.py
@@ -5,7 +5,7 @@ import cv2
 # this is done in individual video frames being processed
 # a webcam feed can be used by passing in 0 for the default webcam 
 # a saved video file uses a string of the filepath
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture('cat.mp4')
 
 # get the first frame
 isTrue, frame = capture.read()
@@ -17,6 +17,8 @@ while isTrue:
     # display the frame
     cv2.imshow('video',frame)
     # get next frame
+    isTrue, frame = capture.read()
+    # resize next frame 
     frame = cv2.resize(frame,(600,400))
     # wait for any keypress and stop if so
     if cv2.waitKey(0):
@@ -24,3 +26,5 @@ while isTrue:
 # stops loading video
 capture.release()
 cv2.destroyAllWindows()
+
+
